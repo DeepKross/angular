599413931f69be70cff2f4bd433292164bf07d18
Mike Tanchuk <volkermischa@gmail.com>
2023-04-22 15:28:47 +0300

    Card component added. Initial data provided. Card type created.

---
+++ src/app/data/cards.ts
@@ -0,0 +1,18 @@
+import {Card} from "../models/card";
+
+export let cards: Card[] = [
+  {
+    id: 1,
+    firstname: "Leanne",
+    lastname: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+},
+{
+    id: 2,
+    firstname: "Ervin",
+    lastname: "Howell",
+    email: "erwin@june,org",
+    phone: "010-692-6593 x09125",
+}
+];
