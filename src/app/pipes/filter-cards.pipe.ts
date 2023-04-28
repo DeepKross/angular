64450a1c43e74b87c58fb3937ab50316bc4b0fd6
Mike Tanchuk <volkermischa@gmail.com>
2023-04-28 21:39:15 +0300

    Bug fixed in filtration pipe that blocked from displaying new card in array

--- src/app/pipes/filter-cards.pipe.ts
+++ src/app/pipes/filter-cards.pipe.ts
@@ -7,6 +7,9 @@ import {Card} from "../models/card";
 export class FilterCardsPipe implements PipeTransform {
 
   transform(cards: Card[], search: string): Card[]{
+
+    if(search === ''){ return cards; }
+
     return cards.filter(p => {
       return p.firstname.toLowerCase().includes(search.toLowerCase()) ||
         p.lastname.toLowerCase().includes(search.toLowerCase())
